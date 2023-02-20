2ba35d049637834fd2de1fdf91208847e73ee0fc
pytholic <rajahaseeb147@gmail.com>
2023-02-20 23:03:24 +0900

    fixed contains duplicate

--- 217.ContainsDuplicate/solution.py
+++ 217.ContainsDuplicate/solution.py
@@ -1,11 +1,21 @@
-def contains_duplicate(nums: list[int]) -> bool:
-    nums_set = set(nums)
-    if len(nums) != len(nums_set):
-        return True
-    else:
-        return False
+# def contains_duplicate(nums: list[int]) -> bool:
+#     nums_set = set(nums)
+#     if len(nums) != len(nums_set):
+#         return True
+#     else:
+#         return False
+
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    hashset = set()
+
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
 
 
 numbers = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-result = contains_duplicate(numbers)
+result = containsDuplicate(numbers)
 print(result)
